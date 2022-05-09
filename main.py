3d02d34049d3df379a9ab64bcfc5ac82943ba46b
Jedrzej_Scelina <jedrzej.scelina@gmail.com>
2022-05-09 18:42:02 +0200

    Zadanie z symbolem Newtona_8

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import math
-dane_czyste=input().strip()
+dane=input().strip()
 # dane_czyste=dane.replace(" ","")
-n=int(dane_czyste[0])
-k=int(dane_czyste[2])
+n=int(dane[0])
+k=int(dane[2])
 
 if k == n:
   print(1)
-
 if k > n:
-    print(0)
-           
+    print(0)        
 else:
     a = math.factorial(n)
     b = math.factorial(k)
